9dff4da230e9fff647667e8e3f9df626836f4e12
Sean O'Hara <sohara@sohara.com>
2010-05-03 10:58:47 -0400

    added localization for dates (e.g. news articles)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,19 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  
+  before_filter :set_locale
+  
+  def set_locale
+    I18n.backend.available_locales
+    I18n.locale = extract_locale_from_uri
+  end
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  def extract_locale_from_uri
+    parsed_locale = request.path.split('/')[1]
+    logger.info "request path is #{request.path}"
+    logger.info "!!!!!!!!!!!!!!! locale is #{parsed_locale}"
+    (I18n.backend.available_locales.include? parsed_locale) ? parsed_locale  : nil
+  end
+ 
 end
