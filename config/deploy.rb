13373ac3207f078a387f36852103e59607ef86de
Sean O'Hara <sohara@sohara.com>
2012-05-28 11:19:38 -0400

    Update capistrano deploy script to use local rvm capistrano integration library

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ require "bundler/capistrano"
 
 #itegration for capistrano with rvm
 set :rvm_type, :user                      # we have RVM in home dir, not system-wide install
-$:.unshift("#{ENV["HOME"]}/.rvm/lib")     # Add RVM's lib directory to the load path.
+$:.unshift("./vendor/lib")     # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, 'ruby-1.8.7'   # Or whatever env you want it to run in.
 #end integration
