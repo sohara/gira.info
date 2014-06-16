40bf15d79e2895032ffde1f262c8f8db1ff942d4
Sean O'Hara <sohara@sohara.com>
2014-06-16 17:51:08 -0400

    Add default encoding to prevent errors

--- config/environment.rb
+++ config/environment.rb
@@ -39,4 +39,8 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  # Prevent template encoding errors
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
 end
